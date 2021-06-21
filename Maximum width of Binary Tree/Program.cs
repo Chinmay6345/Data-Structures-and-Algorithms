1f77873ef27af7ebc1e40a8335d353bedac6248b
Chinmay <Chinmay8116@gmail.com>
2021-06-21 07:45:07 +0530

    commit Maximum_width_of_Binary_Tree

---
+++ Maximum width of Binary Tree/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ttree_Traversals;
+
+namespace Maximum_width_of_Binary_Tree
+{
+    public static class AppHelper
+    {
+        public static Int32 MaxWidth(BinaryTree root)
+        {
+            if(root==null)
+            {
+                return 0;
+            }
+            Int32 res = 0;
+            Queue<BinaryTree> q=new Queue<BinaryTree>();
+            q.Enqueue(root);
+            while(q.Any())
+            {
+                Int32 count = q.Count;
+                res = Math.Max(res, count);
+                for(Int32 i=0;i<count;i++)
+                {
+                    BinaryTree curr = q.Dequeue();
+                    if(curr.left!=null)
+                    {
+                        q.Enqueue(curr.left);
+                    }
+                    if(curr.right!=null)
+                    {
+                        q.Enqueue(curr.right);
+                    }
+                }
+            }
+            return res;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
