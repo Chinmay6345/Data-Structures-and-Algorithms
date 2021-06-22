cd7aa96bae48fa18617ee115ea9dc628f94f9e4f
Chinmay <Chinmay8116@gmail.com>
2021-06-22 06:38:25 +0530

    commit-children sum

---
+++ Children sum property/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Ttree_Traversals;
+namespace Children_sum_property
+{
+    public static class AppHelper
+    {
+        public static bool IsChildrenSum(BinaryTree root)
+        {
+            if (root == null)
+                return true;
+            if (root.left == null && root.right == null)
+                return true;
+            int sum = 0;
+            if (root.left != null) sum += root.left.key;
+            if (root.right != null) sum += root.right.key;
+            return (sum == root.key && IsChildrenSum(root.left) && IsChildrenSum(root.right));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
